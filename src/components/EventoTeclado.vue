6b84fcef910b3f55e6c47682ac0520e2955a5841
AmauriBlanco <blancoamauri@gmail.com>
2024-10-17 16:25:42 -0300

    1-5 Outras possibilidades com teclado

--- src/components/EventoTeclado.vue
+++ src/components/EventoTeclado.vue
@@ -8,17 +8,18 @@ const texto = ref("");
 //     alert(`Tecla pressionada: ${event.key}`);
 // }
 
-// function eventoKeyUp(event) {
-//     texto.value = event.key;
-//     alert(`Tecla pressionada: ${event.key}`);
-// }
+function eventoKeyUp(event) {
+    texto.value = event.key;
+    alert(`Tecla pressionada: ${event.key}`);
+}
 
 function eventoKeyPress(event) {
     texto.value = event.key;
+    alert(`Tecla pressionada: ${event.key}`);
 }
 </script>
 
 <template>
-    <input type="text" @keypress="eventoKeyPress" />
+    <input type="text" @keypress.b="eventoKeyPress" @keyup.a="eventoKeyUp" />
     <p>{{ texto }}</p>
 </template>
