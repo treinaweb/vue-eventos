b079732d873d3b56461d9e7e11f53dfcf4f7ddc4
AmauriBlanco <blancoamauri@gmail.com>
2024-10-19 11:41:09 -0300

    1-9 Iniciando exemplo de Emit

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,11 @@ import EventoClick from "./components/EventoClick.vue";
 import EventoTeclado from "./components/EventoTeclado.vue";
 import EventoMouse from "./components/EventoMouse.vue";
 import Propagacao from "./components/Propagacao.vue";
+import CampoInput from "./components/CompoInput.vue";
+
+import { ref } from "vue";
+
+const nome = ref("");
 </script>
 
 <template>
@@ -22,10 +27,21 @@ import Propagacao from "./components/Propagacao.vue";
         <div>
             <Propagacao />
         </div>
+        <hr />
+        <div>
+            <h2>Emit</h2>
+            <form>
+                <CampoInput/>
+            </form>
+            <p>{{ nome }}</p>
+        </div>
     </main>
 </template>
 
 <style scoped>
+h2 {
+    color: #fff;
+}
 hr {
     margin: 2rem 0;
 }
---
+++ src/components/CompoInput.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <label for="nome">Nome: </label>
+        <input type="text" />
+    </div>
+</template>
